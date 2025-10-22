3f22a5a30bc1230f7bdefbd051a42012e706d7c7
Wallace Gabriel <gs008963@gmail.com>
2025-10-21 21:06:53 -0300

    Prática 2 – Parte 1

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,33 +1,15 @@
 package com.weatherapp
 
-import android.app.Activity
-import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.RoundRect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     //    modifier = Modifier.padding(innerPadding)
                     //)
                     HomePage(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -63,28 +45,7 @@ class MainActivity : ComponentActivity() {
 fun GreetingPreview() {
     WeatherAppTheme {
         // Greeting("Android")
-        HomePage("Android")
+        HomePage()
     }
 }
 
-@Composable
-fun HomePage(name: String, modifier: Modifier = Modifier) {
-
-    val activity = LocalActivity.current as Activity
-
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Bem Vindo")
-        Spacer(modifier = Modifier.size(24.dp))
-        Button(
-            onClick = { activity.finish() }, modifier = Modifier.padding(10.dp)
-        ) {
-            Text(text = "SAIR")
-        }
-    }
-}
